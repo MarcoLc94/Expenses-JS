bf637508546467150fa9da2cdffb43ff1f2df43a
MarcoLc94 <marcoantoniolopezcastro@hotmail.com>
2023-09-30 20:45:38 -0600

    lol

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hola Mundo!");
+const expenses = [
+  {
+    category: "shopping",
+    description: "Nintendo Switch",
+    amount: 500,
+  },
+  {
+    category: "shopping",
+    description: "Tequila",
+    amount: 800,
+  },
+];
