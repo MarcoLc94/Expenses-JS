5c9be9c37c419a0e78ccd03a538fa2f0ef93a781
MarcoLc94 <marcoantoniolopezcastro@hotmail.com>
2023-09-30 19:40:25 -0600

    add new expense

--- navbar.js
+++ navbar.js
@@ -1,5 +1,7 @@
 const navBar = document.querySelector(".js-navbar-menu");
 const nav = document.querySelector(".js-navbar");
+const main = document.querySelector(".js-main");
 navBar.addEventListener("click", () => {
-  navBar.classList.toggle("navbar--open");
+  nav.classList.toggle("navbar--open");
+  main.classList.toggle("section");
 });
