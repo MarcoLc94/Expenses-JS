339f9904378be33a0b54331aac299e0ed19be71e
MarcoLc94 <marcoantoniolopezcastro@hotmail.com>
2023-10-03 18:49:51 -0600

    lol

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const expenses = [
   {
     category: "shopping",
-    description: "Nintendo Switch",
-    amount: 500,
+    description: "Comprar Pan",
+    amount: 5,
   },
   {
     category: "shopping",
@@ -10,3 +10,64 @@ const expenses = [
     amount: 800,
   },
 ];
+
+{
+  /* <li class="expense">
+  <div class="expense__detail">
+    <div>
+      <p class="heading--xs bold">Shopping</p>
+      <p class="content--sm gray-300">Nintendo Switch</p>
+    </div>
+    <p class="content--xl bold">$500</p>
+  </div>
+  <div expense__actions>
+    <a href="#">Delete</a>
+  </div>
+</li>; */
+}
+
+function createExpenseEl(expense) {
+  //create elements
+  const li = document.createElement("li");
+  const expenseDetail = document.createElement("div");
+  const container = document.createElement("div");
+  const category = document.createElement("p");
+  const description = document.createElement("p");
+  const amount = document.createElement("p");
+  const expenseAction = document.createElement("div");
+  const deleteLink = document.createElement("a");
+  //setup element
+  li.classList.add("expense");
+  expenseDetail.classList.add("expense__detail");
+  category.className = "heading--xs bold";
+  category.textContent = expense.category;
+  description.className = "content--sm gray-300";
+  description.textContent = expense.description;
+  amount.className = "content--xl bold";
+  amount.textContent = `$${expense.amount}`;
+  expenseAction.className = "expense__actions";
+  deleteLink.className = "href=#";
+  deleteLink.textContent = "Delete";
+  //build template
+  container.append(category, description);
+  expenseAction.append(deleteLink);
+  expenseDetail.append(container, amount);
+  li.append(expenseDetail, expenseAction);
+  return li;
+}
+
+const expenseList = document.querySelector(".js-expenses");
+const expense = createExpenseEl(expenses[0]);
+expenseList.append(expense);
+
+function renderExpenses(expenses) {
+  const expenseList = document.querySelector(".js-expenses");
+  expenseList.innerHTML = "";
+  expenses.forEach((expense) => {
+    const expenseEl = createExpenseEl(expense);
+    expenseList.append(expenseEl);
+  });
+  //   const listLiExpenses = expenses.map((expense) => createExpenseEl(expense));
+  //   expenseList.append(...listLiExpenses);
+}
+renderExpenses(expenses);
