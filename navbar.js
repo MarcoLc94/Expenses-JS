f228050add380e92d5cff71bbaf1646810f7d5c2
MarcoLc94 <marcoantoniolopezcastro@hotmail.com>
2023-10-11 08:02:52 -0600

    hello

--- navbar.js
+++
@@ -1,7 +0,0 @@
-const navBar = document.querySelector(".js-navbar-menu");
-const nav = document.querySelector(".js-navbar");
-const main = document.querySelector(".js-main");
-navBar.addEventListener("click", () => {
-  nav.classList.toggle("navbar--open");
-  main.classList.toggle("section");
-});
